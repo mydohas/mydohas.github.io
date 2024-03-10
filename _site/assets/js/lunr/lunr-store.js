98d158e25141a29c72b1f8131ef30bf23ef079b2
HaeunDO <HaeunDO>
2024-03-10 18:46:32 +0900

    posting check

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "1주차 리서치",
+        "excerpt":"hi Minimal Mistakes has been developed as a Gem-based theme for easier use, and 100% compatible with GitHub Pages when used as a remote theme. If you enjoy this theme, please consider sponsoring me to continue developing and maintaining it. Installing the theme If you’re running Jekyll v3.7+ and self-hosting...","categories": [],
+        "tags": [],
+        "url": "/grad/research/day1-research",
+        "teaser": null
+      }]
