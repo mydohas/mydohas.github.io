58d2c9c4e64e01e410fbe1b6055ada0c988db06c
HaeunDO <HaeunDO>
2024-03-13 00:35:52 +0900

    w1  리서치 과제

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,12 +1,12 @@
 var store = [{
         "title": "1주차 리서치 1",
-        "excerpt":"졸업프로젝트 주제   ","categories": ["research","grad"],
+        "excerpt":"졸업프로젝트 주제 1. 투약 관련 앱 처방전이나 약봉투의 텍스트를 스캔하여 해당약품의 부작용, 처방 및 조제의 확실성 확인, 함께 먹어도 되는 약인지 확인하는 기능이 담긴 앱. 투약 방법 학습, 약 먹는 시간 체크, 약이 떨어지기 전에 병원에 갈 수 있도록 알림 주는 기능 추가 비슷한 기능이 있는 “스마트DUR”이라는 앱이 있는데 2011년에...","categories": ["research","grad"],
         "tags": [],
         "url": "/research/day1-research",
         "teaser": null
       },{
         "title": "1주차 리서치 2",
-        "excerpt":"채용 정보 리스트업   1 ddmdkr  아니이거 왜안돼   2 ㅏㅣ넝ㄴ미  왜안되  ","categories": ["research","grad"],
+        "excerpt":"채용 정보 리스트업 1. 와이즈텍 직무: 프론트엔드 고용 형태 : 정규직(수습1개월) 자격 요건 : 신입, 경력(5년 이상) 학력 사항 : 고졸 이상 직무 기술 : JS, Vue.js, React.js 우대사항 : 해당직무인턴경력자, 인근거주자 링크 2. 어밸브 직무 : 프론트엔드 고용 형태 : 정규직(수습3개월) 자격 요건 : 신입 학력 사항 : 대졸...","categories": ["research","grad"],
         "tags": [],
         "url": "/research/day1-research2",
         "teaser": null
