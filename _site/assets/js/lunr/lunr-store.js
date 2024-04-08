ecbfed8d490fe0bdcce0089465a7cbb7c53ddd3d
Haeun DO <mydohas@gmail.com>
2024-04-08 17:37:13 +0900

    ADD 0408 w5 research

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "[REDESIGN] PAW-IN-HAND App",
+        "excerpt":"2022년 1학기 인터랙티브 미디어 디자인 1 수업에서 진행한 팀프로젝트      Attruibute         UT: 30%     Design: 30%     Test(Q/A): 70%         Result  ","categories": ["project"],
+        "tags": ["3rd","UIUX"],
+        "url": "/project/paw-in-hand",
+        "teaser": null
+      },{
         "title": "1주차 리서치 1",
         "excerpt":"졸업프로젝트 주제 1. 투약 관련 앱 처방전이나 약봉투의 텍스트를 스캔하여 해당약품의 부작용, 처방 및 조제의 확실성 확인, 함께 먹어도 되는 약인지 확인하는 기능이 담긴 앱. 투약 방법 학습, 약 먹는 시간 체크, 약이 떨어지기 전에 병원에 갈 수 있도록 알림 주는 기능 추가 비슷한 기능이 있는 “스마트DUR”이라는 앱이 있는데 2011년에...","categories": ["research","grad"],
         "tags": [],
@@ -11,9 +17,27 @@ var store = [{
         "url": "/research/day1-research2",
         "teaser": null
       },{
-        "title": "[REDESIGN] PAW-IN-HAND App",
-        "excerpt":"2022년 1학기 인터랙티브 미디어 디자인 1 수업에서 진행한 팀프로젝트      Attruibute         UT: 30%     Design: 30%     Test(Q/A): 70%         Result  ","categories": ["project"],
-        "tags": ["3rd","UIUX"],
-        "url": "/project/paw-in-hand",
+        "title": "2주차 아이데이션",
+        "excerpt":"프로젝트 아이데이션 라이더 안전배달 도로문화 개선 어플, 모두가 기분 좋은 안전한 배달문화 어떻게 하면 라이더가 교통법규를 잘 지키게 하면서, 모두가 만족하는 배달 문화를 만들 수 있을까? 이해관계: 배달하는 자, 배달 받는 자, 도로에 있는 자 (제3자) painpoint: 배달 받는 고객이 빨리 음식을 받고자 하는 니즈가 있다보니 + 정해진 시간 내에...","categories": ["research","grad"],
+        "tags": [],
+        "url": "/research/w2",
+        "teaser": null
+      },{
+        "title": "3주차",
+        "excerpt":"리서치#1 - 라이더와 소비자가 모두 만족하는 안전한 배달문화 만들기! 배달 상황의 전체적인 플로우를 리서치를 해봤고, 가게 사장, 배달원, 소비자의 입장에 대해 리서치를 했을때 플랫폼 구조상 다양한 이해관계에 얽혀서 사회 구조적인 문제가 있기도 하였다. 리서치 결과 ➡️ 환경 배달쓰레기 문제, 배달원 안전 사고, 수수료 문제, 지방 수도권 문제 등 그중 배달 사고...","categories": ["research","grad"],
+        "tags": [],
+        "url": "/research/w3",
+        "teaser": null
+      },{
+        "title": "4주차",
+        "excerpt":"리서치#2 - 점주, 라이더, 소비자 입장을 중심으로..    ","categories": ["research","grad"],
+        "tags": [],
+        "url": "/research/w4",
+        "teaser": null
+      },{
+        "title": "5주차",
+        "excerpt":"리서치#3 - 유저 인터뷰     ","categories": ["research","grad"],
+        "tags": [],
+        "url": "/research/w5",
         "teaser": null
       }]
