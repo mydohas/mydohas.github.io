76faeef6a4b459f1a27dce472c8f61420a18a1a9
DOHA <mydohas@gmail.com>
2024-04-29 13:24:41 +0900

    0429 add all thumbnails

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -9,37 +9,37 @@ var store = [{
         "excerpt":"졸업프로젝트 주제 1. 투약 관련 앱 처방전이나 약봉투의 텍스트를 스캔하여 해당약품의 부작용, 처방 및 조제의 확실성 확인, 함께 먹어도 되는 약인지 확인하는 기능이 담긴 앱. 투약 방법 학습, 약 먹는 시간 체크, 약이 떨어지기 전에 병원에 갈 수 있도록 알림 주는 기능 추가 비슷한 기능이 있는 “스마트DUR”이라는 앱이 있는데 2011년에...","categories": ["research","grad"],
         "tags": [],
         "url": "/research/day1-research",
-        "teaser": null
+        "teaser": "/assets/img/grad/thumbnails/w1-2.jpg"
       },{
         "title": "1주차 리서치 2",
         "excerpt":"채용 정보 리스트업 1. 와이즈텍 직무: 프론트엔드 고용 형태 : 정규직(수습1개월) 자격 요건 : 신입, 경력(5년 이상) 학력 사항 : 고졸 이상 직무 기술 : JS, Vue.js, React.js 우대사항 : 해당직무인턴경력자, 인근거주자 링크 2. 어밸브 직무 : 프론트엔드 고용 형태 : 정규직(수습3개월) 자격 요건 : 신입 학력 사항 : 대졸...","categories": ["research","grad"],
         "tags": [],
         "url": "/research/day1-research2",
-        "teaser": null
+        "teaser": "/assets/img/grad/thumbnails/w1-1.jpg"
       },{
         "title": "2주차 아이데이션",
         "excerpt":"프로젝트 아이데이션 라이더 안전배달 도로문화 개선 어플, 모두가 기분 좋은 안전한 배달문화 어떻게 하면 라이더가 교통법규를 잘 지키게 하면서, 모두가 만족하는 배달 문화를 만들 수 있을까? 이해관계: 배달하는 자, 배달 받는 자, 도로에 있는 자 (제3자) painpoint: 배달 받는 고객이 빨리 음식을 받고자 하는 니즈가 있다보니 + 정해진 시간 내에...","categories": ["research","grad"],
         "tags": [],
         "url": "/research/w2",
-        "teaser": null
+        "teaser": "/assets/img/grad/thumbnails/w2.jpg"
       },{
         "title": "3주차",
         "excerpt":"리서치#1 - 라이더와 소비자가 모두 만족하는 안전한 배달문화 만들기! 배달 상황의 전체적인 플로우를 리서치를 해봤고, 가게 사장, 배달원, 소비자의 입장에 대해 리서치를 했을때 플랫폼 구조상 다양한 이해관계에 얽혀서 사회 구조적인 문제가 있기도 하였다. 리서치 결과 ➡️ 환경 배달쓰레기 문제, 배달원 안전 사고, 수수료 문제, 지방 수도권 문제 등 그중 배달 사고...","categories": ["research","grad"],
         "tags": [],
         "url": "/research/w3",
-        "teaser": null
+        "teaser": "/assets/img/grad/thumbnails/w3.jpg"
       },{
         "title": "4주차",
         "excerpt":"리서치#2 - 점주, 라이더, 소비자 입장을 중심으로..    ","categories": ["research","grad"],
         "tags": [],
         "url": "/research/w4",
-        "teaser": null
+        "teaser": "/assets/img/grad/thumbnails/w4.jpg"
       },{
         "title": "5주차",
         "excerpt":"리서치#3 - 유저 인터뷰     ","categories": ["research","grad"],
         "tags": [],
         "url": "/research/w5",
-        "teaser": null
+        "teaser": "/assets/img/grad/thumbnails/w5.jpg"
       },{
         "title": "6주차",
         "excerpt":"인터뷰 분석 및 행동 패턴 분석   이번 주 스터디 링크 -&gt; Next Week… Persona 작성  ","categories": ["research","grad"],
@@ -51,7 +51,7 @@ var store = [{
         "excerpt":"아이데이션 정리 및 페르소나 작성 + 스토리보드 제작             이번 주 스터디 링크   -&gt; Next Week… User Journey 작성  ","categories": ["research","grad"],
         "tags": [],
         "url": "/research/w7",
-        "teaser": "/assets/img/grad/w7-2.png"
+        "teaser": "/assets/img/grad/thumbnails/w7.jpg"
       },{
         "title": "8주차",
         "excerpt":"중간발표   지금까지 진행한 내용을 발표      Behance  ","categories": ["research","grad"],
